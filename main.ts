15d3c6074b6e8dca70fbbf02e43891e04a1c580b
aaronmyatt <aaronmyatt@gmail.com>
2020-11-05 16:07:29 +0800

    2 files modified, 1 added

---
+++ main.ts
@@ -0,0 +1,16 @@
+import { db, Contact, Encounter, Plan } from "./database";
+import { ContactService, EncounterService, PlanService } from "./services";
+
+export const database = db;
+
+export const models = {
+  Contact,
+  Encounter,
+  Plan
+};
+
+export const services = {
+  contactService: new ContactService(),
+  encounterService: new EncounterService(),
+  planService: new PlanService()
+};
